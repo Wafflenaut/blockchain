b96417b3efc02e7ba56b232b3ec59452f210ab4e
jmoyer <moyerjo@oregonstate.edu>
2018-05-08 14:38:39 -0400

    Added block mining/block chain and block testing

---
+++ blockchain.js
@@ -0,0 +1,13 @@
+const Block = require('./block');
+
+class Blockchain {
+	constructor() {
+		this.chain = [Block.genesis()];
+	}
+	
+	addBlock(data) {
+		const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+		this.chain.push(block);
+		
+	}
+}
